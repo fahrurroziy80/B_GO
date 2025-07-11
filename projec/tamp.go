ec8df163fb957d8905566165bfea003775eb4744
Fahrurrozy <rrozif96@gmail.com>
2025-07-11 19:08:47 +0700

    belajar go

---
+++ projec/tamp.go
@@ -0,0 +1,21 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var nama string = "fahrurrozi"
+// 	umur := 20
+// 	var alamat string = "lombok"
+
+// 	fmt.Println("nama saya ", nama)
+// 	fmt.Println("Umur saya", umur, "tahun.")
+// 	fmt.Println("Alamat saya di", alamat)
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Halo, Go!")
+}
